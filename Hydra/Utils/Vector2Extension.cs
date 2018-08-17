6e539b917719825fbc19ee69b7e803595c4f0e1a
Pablo Henrique Bertaco <pbertaco@umov.me>
2018-08-17 18:32:23 -0300

    Add Vector2Extension

---
+++ Hydra/Utils/Vector2Extension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Hydra
+{
+    public static class Vector2Extension
+    {
+        public static float distanceTo(this Vector2 vector2, Vector2 position)
+        {
+            return Vector2.Distance(vector2, position);
+        }
+    }
+}
