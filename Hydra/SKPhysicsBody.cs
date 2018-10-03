70f82e02b10de2532fcf49cae387d0dcde9c39a8
Pablo Henrique Bertaco <pbertaco@umov.me>
2018-10-03 09:27:50 -0300

    Add EdgeShape and ChainShape to SKPhysicsBody

--- Hydra/SKPhysicsBody.cs
+++ Hydra/SKPhysicsBody.cs
@@ -49,19 +49,39 @@ namespace Hydra
                 case ShapeType.Polygon:
                     throw new NotImplementedException();
                 case ShapeType.Chain:
-                    throw new NotImplementedException();
+                    vertices = PolygonTools.CreateRectangle(size.X / 2, size.Y / 2);
+                    break;
                 case ShapeType.TypeCount:
                     throw new NotImplementedException();
             }
 
-            load(vertices);
+            load(vertices, shapeType);
         }
 
-        void load(Vertices vertices)
+        void load(Vertices vertices, ShapeType shapeType = ShapeType.Polygon)
         {
-            vertices.Scale(ConvertUnits.ToSimUnits(Vector2.One));
-            PolygonShape polygonShape = new PolygonShape(vertices, 1.0f);
-            CreateFixture(polygonShape);
+        	vertices.Scale(ConvertUnits.ToSimUnits(Vector2.One));
+        	
+            switch (shapeType)
+            {
+                case ShapeType.Unknown:
+                    loadPolygonShape(vertices);
+                    break;
+                case ShapeType.Circle:
+                    loadPolygonShape(vertices);
+                    break;
+                case ShapeType.Edge:
+                    loadEdgeShape(vertices);
+                    return;
+                case ShapeType.Polygon:
+                    loadPolygonShape(vertices);
+                    break;
+                case ShapeType.Chain:
+                    loadChainShape(vertices);
+                    break;
+                case ShapeType.TypeCount:
+                    throw new NotImplementedException();
+            }
 
             BodyType = BodyType.Dynamic;
 
@@ -72,6 +92,26 @@ namespace Hydra
             AngularDamping = 1.0f;
             Restitution = 0.25f;
             Friction = 0.125f;
+        }
+
+        void loadEdgeShape(Vertices vertices)
+        {
+            Vector2 start = vertices[0];
+            Vector2 end = vertices[1];
+            EdgeShape shape = new EdgeShape(start, end);
+            CreateFixture(shape);
+        }
+
+        void loadPolygonShape(Vertices vertices)
+        {
+            PolygonShape shape = new PolygonShape(vertices, 1.0f);
+            CreateFixture(shape);
+        }
+
+        void loadChainShape(Vertices vertices)
+        {
+            ChainShape shape = new ChainShape(vertices, true);
+            CreateFixture(shape);
         }
     }
 }
